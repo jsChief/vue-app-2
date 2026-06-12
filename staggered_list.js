f1c70e9b0218ded25a63d49309c66d2e1a8c95cc
jsChief <doctorpyro15@gmail.com>
2026-06-12 18:31:03 +0100

    Removed unnecessary files and stashed stuff

--- staggered_list.js
+++
@@ -1,8 +0,0 @@
- function replay(id) {
-   const list = document.getElementById(id);
-   list.querySelectorAll('[class*="item"]').forEach((el) => {
-     el.style.animation = 'none';
-     el.offsetHeight; // reflow
-     el.style.animation = '';
-   });
- }
